979d9cac0ad228ef7799912e767671d162b46a0a
stephanetapasco <stephanetapascom@hotmail.com>
2022-06-27 11:35:15 +0200

    commit apres avoir perdu les commentaire sur la bases de donnée, code valentino (main)

---
+++ app/Http/Controllers/HealthinfoController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class HealthinfoController extends Controller
+{
+    public function formulaire()
+    {
+        return view('val.edit-healthinfo');
+    }
+
+    public function traitement() {
+        request()->validate([
+            'firstname' => ['required', 'max:50', 'alpha'],
+            'lastname' => ['required', 'max:50', 'alpha'],
+            'age' => ['required','max:3', 'alpha_num'],
+            'nameContact' => ['required','max:50', 'alpha'],
+            'phoneContact' => ['required','max:10', 'alpha_num'],
+            'blood' => ['required','max:5'],
+            'doctor' => ['required','max:10', 'alpha_num'],
+            'size' => ['required','max:3', 'alpha_num'],
+            'weight' => ['required','max:3', 'alpha_num'],
+            'other' => ['max:200'],
+            'photo' => ['max:100'],
+        ]);
+
+        auth()->user()->update([
+            'firstname'=>request('firstname'),
+            'lastname'=>request('lastname'),
+            'age'=>request('age'),
+            'nameContact'=>request('nameContact'),
+            'phoneContact'=>request('phoneContact'),
+            'blood'=>request('blood'),
+            'size'=>request('size'),
+            'weight'=>request('weight'),
+            'doctor'=>request('doctor'),
+            'other'=>request('other'),
+            'photo'=>request('photo'),
+        ]);
+
+        return redirect('/myaccount');
+    }
+}
---
+++ database/migrations/2022_06_22_132731_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('password_confirmation');
+            $table->string('lastname')->nullable();
+            $table->string('firstname')->nullable();
+            $table->string('age')->nullable();
+            $table->string('blood')->nullable();
+            $table->string('phoneContact')->nullable();
+            $table->string('nameContact')->nullable();
+            $table->string('weight')->nullable();
+            $table->string('size')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('doctor')->nullable();
+            $table->string('other')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
