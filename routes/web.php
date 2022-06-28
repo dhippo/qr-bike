f5cc8966d5bbc2f4f318295c228572fc001d7013
dhippo <durandhippolyte@gmail.com>
2022-06-28 12:09:27 +0200

    more settings for mailgun and test mail template (not working)

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,28 @@ Route::post('/signin', [SigninController::class, 'traitement']);
 
 Route::get('/myaccount', [MyaccountController::class, 'traitement']);
 
-Route::view('/', 'welcome');
+//Route::view('/', 'welcome');
+Route::get('/', 'App\Http\Controllers\ContactMailController');
+//use App\Http\Requests\ContactRequest;
+//use App\Http\Controllers\ContactMailController;
 
 Route::get('/edit-healthinfo', [HealthinfoController::class, 'formulaire']);
 
 Route::post('/edit-healthinfo', [HealthinfoController::class, 'traitement']);
+
+Route::view('/firstemail', 'firstemail');
+
+Route::get("send-email", [EmailController::class, "sendEmail"]);
+
+
+
+Route::middleware(['guest'])->group(function(){
+
+    Route::get('/', [ContactMailController::class, 'index'])->name('contact');
+
+    Route::post('/contact', [ContactMailController::class, 'contactSendMail'])->name('contact.send');
+
+});
 
 
 
