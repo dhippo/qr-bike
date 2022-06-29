4dc86ddc42bd0447ad4d098dedeb59ef7ea149f4
dhippo <durandhippolyte@gmail.com>
2022-06-29 12:43:50 +0200

    MAIL SEND !!! with Mail::to()->send()
    try the command line ./vendor/bin/sail artisan hippo:testemail

---
+++ app/Console/Commands/TestEmail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\CheckEmail;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hippo:testemail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Email !!!');
+        Mail::to('durandhippolyte@gmail.com')->send(new CheckEmail());
+        return 0;
+    }
+}
