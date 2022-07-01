dfd2861467a555af87130382c2b95b44beb895ea
stephanetapasco <stephanetapascom@hotmail.com>
2022-07-01 15:02:26 +0200

    model user update

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,35 +5,75 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Auth\Authenticatable as BasicAuthenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
 
 class User extends Model implements Authenticatable
 {
     use BasicAuthenticatable;
+    /*namespace App\Models;use Illuminate\Contracts\Auth\Authenticatable;use Illuminate\Auth\Authenticatable as BasicAuthenticatable;class User extends Model implements Authenticatable{use BasicAuthenticatable;*/
 
     protected $fillable = [
+
+        /* 1 */
+
         'email',
         'password',
-        'password_confirmation',
         'token',
-        'utilisateur_id',
+        'active_token',
+
+        /* Profil */
+
+        'sex',
         'firstname',
+        'infos',
         'lastname',
-        'phoneContact',
-        'nameContact',
+        'phone',
+        'country',
+        'city',
         'photo',
+
+        /* Health infos */
+
         'age',
         'size',
         'weight',
         'blood',
         'other',
+
+        /* Contacts */
+
+        'nameContact',
+        'phoneContact',
         'nameDoctor',
         'phoneDoctor',
-        'sex',
-        'city',
-        'country',
-        'active_token',
-        ];
+
+    ];
+
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        // 'password2',
+        // 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+
+        /* 1 */
+
+        // 'email_verified_at' => 'datetime',
+
+        'infos' => 'array',
+    ];
 
     public $timestamps = false;
 
