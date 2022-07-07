3bcf280bc84dfef7d517b40fe4599e836491f8bc
dhippo <durandhippolyte@gmail.com>
2022-07-07 17:19:56 +0200

    simple header on layouts.homepage

--- resources/views/layouts/homepages.blade.php
+++ resources/views/layouts/homepages.blade.php
@@ -18,51 +18,40 @@
 
 <!--====== HEADER PART  ======-->
 
-    <nav class="fixed flex justify-between py-6 w-full lg:px-48 md:px-12 px-4 content-center z-10"
-         style="background-color: rgba(23,67,99,255)">
-        <div class="flex items-center">
-        </div>
-        <ul class="font-montserrat items-center hidden md:flex">
-            <li class="mx-3 ">
-                <a class="growing-underline" href="#">
-                    Home
-                </a>
-            </li>
-            <li class="growing-underline mx-3">
-                <a href="#">Services</a>
-            </li>
-            <li class="growing-underline mx-3">
-                <a href="#">About us</a>
-            </li>
-        </ul>
-        <div class="font-montserrat hidden md:block">
-            <button class="mr-6"><a href="#">Login</a></button>
-            <button class="py-2 px-4 text-white bg-black rounded-3xl">
-                <a href="#">Get started</a>
-            </button>
-        </div>
-        <div id="showMenu" class="md:hidden">
+<header style="width: 100vw; position: fixed; background-color: rgba(23,67,99,255); border-radius: 15px">
+    <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+        <div class="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+            <div class="flex items-center">
+                <a href="#" class="text-base font-medium text-white hover:text-indigo-50"> Home </a>
+                {{-- LOGO HERE
+                <a href="#">
+                    <img class="h-10 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white" alt="">
+                </a>--}}
+                <div class="hidden space-x-8 lg:block"
+                     style="margin-left: 5em">
 
+                    <a href="#" class="text-base font-medium text-white hover:text-indigo-50"> Services </a>
+
+                    <a href="#" class="text-base font-medium text-white hover:text-indigo-50"> About us </a>
+
+                    <a href="#" class="text-base font-medium text-white hover:text-indigo-50"> Shop </a>
+                </div>
+            </div>
+            <div class="ml-10 space-x-4">
+                <a href="#" class="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75">Sign in</a>
+                <a href="#" class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium hover:bg-indigo-50" style="color:rgba(23,67,99,255)">Sign up</a>
+            </div>
+        </div>
+        <div style="margin-top: 1px" class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+
+            <a href="#" style="margin-left: 1em" class="text-base font-medium text-white hover:text-indigo-50"> Services </a>
+
+            <a href="#" style="margin-left: 1em" class="text-base font-medium text-white hover:text-indigo-50"> About us </a>
+
+            <a href="#" style="margin-left: 1em" class="text-base font-medium text-white hover:text-indigo-50"> Shop </a>
         </div>
     </nav>
-    <div id='mobileNav' class="hidden px-4 py-6 fixed top-0 left-0 h-full w-full bg-secondary z-20 animate-fade-in-down">
-        <div id="hideMenu" class="flex justify-end">
-            <img src='dist/assets/logos/Cross.svg' alt="" class="h-16 w-16" />
-        </div>
-        <ul class="font-montserrat flex flex-col mx-8 my-24 items-center text-3xl">
-            <li class="mx-3 ">
-                <a class="growing-underline" href="#">
-                    Home
-                </a>
-            </li>
-            <li class="growing-underline mx-3">
-                <a href="#">Services</a>
-            </li>
-            <li class="growing-underline mx-3">
-                <a href="#">About us</a>
-            </li>
-        </ul>
-    </div>
+</header>
 
 <!--====== END OF HEADER ======-->
 
