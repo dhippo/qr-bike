4763405983ea8505e91f58a361ffd53a09101e2a
stephanetapasco <stephanetapascom@hotmail.com>
2022-07-07 15:40:33 +0200

    add comment only

--- resources/views/homepages/home.blade.php
+++ resources/views/homepages/home.blade.php
@@ -2,6 +2,29 @@
 
 @section('content')
 
-    <h1>HOME PAGE</h1>
+    <section
+            class="pt-24 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-48 md:px-12 px-4 bg-secondary">
+        <div class="md:flex-1 md:mr-10">
+            <h1 class="font-pt-serif text-5xl font-bold mb-7">
+                A headline for your
+                <span class="bg-underline1 bg-left-bottom bg-no-repeat pb-2 bg-100%">
+          cool website
+        </span>
+            </h1>
+            <p class="font-pt-serif font-normal mb-7">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum harum
+                tempore consectetur voluptas, cumque nobis laboriosam voluptatem.
+            </p>
+            <div class="font-montserrat">
+                <button class="bg-black px-6 py-4 rounded-lg border-2 border-black border-solid text-white mr-2 mb-2">
+                    Call to action
+                </button>
+                <button class="px-6 py-4 border-2 border-black border-solid rounded-lg">
+                    Secondary action
+                </button>
+            </div>
+        </div>
+
+    </section>
 
 @endsection
