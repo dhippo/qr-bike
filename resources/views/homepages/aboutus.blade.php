5c36649b5afab950b7d9264f5fcd78c061d22f3a
dhippo <durandhippolyte@gmail.com>
2022-07-08 16:29:49 +0200

    aztry

--- resources/views/homepages/aboutus.blade.php
+++ resources/views/homepages/aboutus.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-    <main class="min-h-full bg-cover bg-top sm:bg-top" style="background-image: url('https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75')">
-        <div class="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
+    <main class="min-h-full bg-cover bg-top sm:bg-top invert-0" style="background-image: url('{{URL::asset('/images/bgqrcode.jpg')}}')">
+        <div class="max-w-7xl mx-auto px-4 py-16 text-center text-invert sm:px-6 sm:py-24 lg:px-8 lg:py-48">
             <p class="text-sm font-semibold text-black text-opacity-50 uppercase tracking-wide">MYQRCODE.INFO</p>
-            <h1 class="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">We hope you enjoy generating QR CODE with our website.</h1>
+            <h1 class="mt-2 text-4xl font-extrabold text-invert tracking-tight sm:text-5xl">We hope you enjoy generating QR CODE with our website.</h1>
             <p class="mt-2 text-lg font-medium text-black text-opacity-50">It looks like you looking for information about us.</p>
             <div class="mt-6">
                 <a href="#" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50"> Go back home </a>
