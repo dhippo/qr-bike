b268d92eade6072ca33dca0e6796000db2d6a219
jacinto_lebo <jacinto.valentino27@gmail.com>
2022-07-20 17:11:32 +0200

    version main 20 juillet

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-    <div style="background-color: rgba(186,217,234,255)" class="h-screen w-screen overflow-hidden">
-        <div class="relative bg-white "style="background-color: rgba(186,217,234,255); z-index: 3">
+    <div style="background-color: rgba(255,245,232,255)" class="h-screen w-screen overflow-hidden">
+        <div class="relative bg-white "style="background-color: rgba(255,245,232,255); z-index: 3">
             <div class="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
                 <div class="flex justify-start lg:w-0 lg:flex-1">
                     <a href="#">
@@ -21,8 +21,8 @@
 
                 </nav>
                 <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                    <a href="/signin" style="background-color: rgba(23,67,99,255)" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign in </a>
-                    <a href="/signup" class=" px-4 py-2 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900" > Sign up </a>
+                    <a href="/signin" style="background-color: rgba(249,191,110,255)" class="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign in </a>
+                    <a href="/signup" class="ml-4 px-4 py-2 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900" > Sign up </a>
 
                 </div>
             </div>
@@ -31,7 +31,7 @@
         <div class="min-h-full flex ">
 
             <div style="z-index: 2" class="hidden lg:block relative w-0 flex-1">
-                <img style="margin-left: -180px; margin-top: -50px" class="absolute inset-0 w-full object-cover h-screen" src="{{URL::asset('/images/artqr.png')}}"
+                <img style="width: 750px; height: 750px; margin-left: 140px; margin-top: 20px" class="absolute inset-0 w-full object-cover h-screen" src="{{URL::asset('/images/qrjaune.png')}}"
                      alt="">
             </div>
 
@@ -113,7 +113,7 @@
                                     </div>
                                 </div>
                                 <div class="flex">
-                                    <button style="background-color: rgba(23,67,99,255); border-radius: 50px" type="submit"
+                                    <button style="background-color: rgba(249,191,110,255); border-radius: 50px" type="submit"
                                             class="rounded-full h-14 w-full mt-6 mr-auto h-10 justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                         Sign up
                                     </button>
